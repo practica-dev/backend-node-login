5bbf12bac3035ff504c53855607f83b6a52d77e8
Manuel Florez <manuelflorezw@gmail.com>
2024-01-11 18:11:30 -0500

    se agrega codigo básico de prueba sobre express

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hola Mundo!");
+});
+
+app.listen(3000, function () {
+  console.log("Aplicación ejemplo, escuchando el puerto 3000!");
+});
