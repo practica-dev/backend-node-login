720208cd77a48fcf04774af4bbaed0b4a9107fc6
Manuel Florez <manuelflorezw@gmail.com>
2024-01-18 11:10:30 -0500

    add basic use express().json()

--- src/index.js
+++ src/index.js
@@ -6,13 +6,12 @@ const port = 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  const { user, password } = req.body;
-  console.log({ user, password });
   res.send("Hola Mundo!");
 });
 
 app.post('/login', (req, res) => {
-  console.log(req.body);
+  const { user, password } = req.body;
+  console.log({ user, password });
   res.send("login!");
 });
 
